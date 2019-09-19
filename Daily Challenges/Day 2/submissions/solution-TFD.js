c9fd3ba78066cc833a74e726b8b31929139a99da
thefrustratedDeveloper <dhruvsaaaxena.1998@gmail.com>
2019-09-19 21:37:22 +0530

    Solution of Day 2 - TFD | Added

--- Daily Challenges/Day 2/submissions/solution-TFD.js
+++ Daily Challenges/Day 2/submissions/solution-TFD.js
@@ -10,11 +10,16 @@ const twosum = (nums, target) => {
 }
 
 // T: Test Case 1 
-console.log(twosum([2, 7, 11, 19], 9));
-// Expected  : [0,1]
-// Delivered : [0,1]
+console.log(twosum([3, 6, 8, 3, 4], 11));
+// Expected  : [0,2]
+// Delivered : [0,2]
 
 // T: Test Case 2
 console.log(twosum([3, 2, 4], 6));
-    // Expected  : [1,2]
-    // Delivered : [1,2]
+// Expected  : [1,2]
+// Delivered : [1,2]
+
+// T: Test Case 2
+console.log(twosum([2, 7, 11, 15], 9));
+    // Expected  : [0,1]
+    // Delivered : [0,1]
