ea22399b5bd9fc8d4eca5476bef949b411a42326
thefrustratedDeveloper <dhruvsaaaxena.1998@gmail.com>
2019-09-19 21:23:36 +0530

    Solution - TFD | Added

--- Daily Challenges/Day 2/submissions/solution-TFD.js
+++ Daily Challenges/Day 2/submissions/solution-TFD.js
@@ -1,1 +1,20 @@
-// TODO : Solution
+const twosum = (nums, target) => {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+
+}
+
+// T: Test Case 1 
+console.log(twosum([2, 7, 11, 19], 9));
+// Expected  : [0,1]
+// Delivered : [0,1]
+
+// T: Test Case 2
+console.log(twosum([3, 2, 4], 6));
+    // Expected  : [1,2]
+    // Delivered : [1,2]
