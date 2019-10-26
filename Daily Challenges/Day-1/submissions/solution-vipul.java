a84798b85a538f7888b01e859784769fff047cde
Vipul Jha <vipuljha08@gmail.com>
2019-10-26 17:31:13 +0530

    Day1: Add my day 1 solution
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

---
+++ Daily Challenges/Day-1/submissions/solution-vipul.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+class solutionDay1 {
+
+    public static void main(String[] args) {
+
+        // Declaring variables
+        int[][] matrix = new int[3][3];
+        int i, j, sumLeftDiagonal = 0, sumRightDiagonal = 0;
+
+        // Calling scanner class
+        Scanner scan = new Scanner(System.in);
+
+        // Try and finally to close scan variable if needed
+        try {
+            System.out.print("Enter the elements of 3x3 matrix: ");
+
+            // Take input of row and column
+            for (i = 0; i < 3; i++) {
+                for (j = 0; j < 3; j++) {
+                    matrix[i][j] = scan.nextInt();
+                }
+            }
+        } finally {
+            scan.close();
+        }
+        System.out.println("");
+        System.out.println("========================");
+        System.out.println("");
+
+        // Print the matrix elements
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+
+            System.out.println();
+        }
+
+        // Scan through matrix to find sum of left and right diagonal
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                if (i == j) {
+                    sumLeftDiagonal = sumLeftDiagonal + matrix[i][j];
+                }
+
+                if (i == 3 - j - 1) {
+                    sumRightDiagonal = sumRightDiagonal + matrix[i][j];
+                }
+            }
+        }
+
+        // Print all the solutions
+        System.out.println("");
+        System.out.println("========================");
+        System.out.println("");
+        System.out.println("Sum of left diagonal: " + sumLeftDiagonal);
+        System.out.println("Sum of right diagonal: " + sumRightDiagonal);
+        System.out.println("Subtraction of sum of left and right diagonals: " + (sumLeftDiagonal - sumRightDiagonal));
+
+    }
+}
